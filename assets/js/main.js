733cd3a2d467ea7f0ec6802e3fa8ec2cebd71aaa
mabior7 <102356700+mabior7@users.noreply.github.com>
2022-06-18 21:16:52 +0300

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "12 June 2022";
+const newYears = "12 June 2023";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
